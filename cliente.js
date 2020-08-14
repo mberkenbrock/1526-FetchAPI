52935e9b48e8607b2061e0b94e5e42b5cb2ff4c0
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-08-13 21:28:59 -0300

    Add files via upload

---
+++ cliente.js
@@ -0,0 +1,20 @@
+const informacoesClinetes = [
+    {
+    nome : 'Marlon',
+    cpf  : '07078010957'
+    }
+]
+
+
+const conteudoLinha = 
+`<tr>
+        <td>66809433023</td>
+        <td>Jairo</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>18875539081</td>
+        <td>Elena</td>
+      </tr>`;
+   
+   console.log('conteudo linha');
